505899f89c82823051a50d26b6a49c4d9e9cc847
SergioRocha92 <69970438+SergioRocha92@users.noreply.github.com>
2020-12-05 20:33:04 -0300

    Update HomeScreen.js

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,99 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
-export class HomeScreen extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          name: '',
-          img: '#',
+export  class HomeScreen extends Component {
+
+constructor(props){
+    super(props);
+
+    this.state = {
+        loading: false,
+        pokemon: [],
+        url: 'https://pokeapi.co/api/v2/pokemon/'
+
+    }
+
+}
+
+componentDidMount(){
+    this.getPokemon();
+}
+    getPokemon = () => {
+
+        this.setState({ loading:true})
+
+        fetch(this.state.url)
+        .then(res => res.json() )
+        .then( res => {
+
+            this.setState({
+                pokemon : res.results,
+                url: res.next,
+                loading: false
+
+            })
+
+        })
+
+    };
+
+
+
+    render(){
+        if(this.state.loading){
+
+            return(
+                <View style={styles.container}>
+                    <Text>Descargando Pokemon</Text>
+                    <StatusBar style="auto" />
+                </View>
+            );
+
         }
-      }
-      
-     
-    
-      fetchJale = async () => {
-      
-        let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.name}`)
-        let data = await res.json()
-    
-        
-        console.log(data.name);
-        console.log(data.sprites.front_default)
-        this.setState({
-          img: data.sprites.front_default
-        })
-      }
-      
-    
-      handleName = event => {
-        this.setState({
-          name: event.target.value
-        })
-      }
-      
-    
-      handleSubmit = event =>{
-        var jeje = this.state.name;
-        console.log(jeje)
-        alert(jeje)
-        this.fetchJale()
-        
-        this.setState({
-          name: '',
-        })
-        event.preventDefault()
-      }
-      
-    
-    
-    
-    
-    
-    
-    /* render(){
+
+
         return(
-            <View style={styles.container}>
-                <Text>This is the home screen</Text>
-                <StatusBar style="auto" />
+            <View style={{flex: 1, paddingTop:50, paddingLeft:5}}>
+                <Text>Pokemon</Text>
+
+                <Image
+          style={styles.tinyLogo}
+          source={{uri: 'https://davidquevedo.neocities.org/imagenes/PokeLogo2.png'}}
+        />
+
+                
+                
+                <FlatList
+                data={this.state.pokemon}
+                renderItem={
+                    ({item}) => <Text> { item.name } </Text>
+                    
+            
+                }
+                keyExtractor={(item, index) => index.toString()  }
+
+                
+                />
+               
             </View>
         );
-    }*/ 
+        
+    }
     
-
-   render() {
-    return (
-    <div className="card text-center">
-      <div className="card-header">
-        Pokedex
-      </div>
-      <div className="card-body">
-    <form onSubmit={this.handleSubmit}>    
-        <label>Ingrese el pokemon</label><br/>
-        <input 
-            type="text"
-            placeholder="nombre"
-            value={this.state.name}
-            onChange={this.handleName}
-        /><br/><br/>
-        <button type="submit" className="btn btn-primary">submit</button>
-    </form>
-    <img src={this.state.img}/>
-      </div>
-      
-    </div>
-    );
-} 
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+  }
+  
+  ); 
